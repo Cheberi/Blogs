457c55b02256150e4b7cffd41e361a24f0ab5567
Cheberi <meshackkemboi011@gmail.com>
2022-05-18 20:17:41 +0300

    created a user for test-user

--- tests/test_post.py
+++ tests/test_post.py
@@ -4,11 +4,11 @@ from app.models import Post, User, Comment
 class TestPost(unittest.TestCase):
     
     def setUp(self):
-        self.user_Collins = User(first_name = "Collins",
-                                last_name = "Muriuki",
-                                username = "collins_m",
-                                password = "easy",
-                                email = "collins@mail.com")
+        self.user_Collins = User(first_name = "Meshack",
+                                last_name = "Cheberi",
+                                username = "Cheberi",
+                                password = "Meshack011.",
+                                email = "meshackkemboi011@mail.com")
         self.new_post = Post(post_title = "Sample Title",
                             post_content = "Hallo Welt! Ich bin hier",
                             user_id = self.user_Collins.id)
@@ -17,6 +17,6 @@ class TestPost(unittest.TestCase):
                                     user_id = self.user_Collins.id)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.user_Collins, User))
+        self.assertTrue(isinstance(self.user_Cheberi, User))
         self.assertTrue(isinstance(self.new_post, Post))
         self.assertTrue(isinstance(self.new_comment, Comment))
